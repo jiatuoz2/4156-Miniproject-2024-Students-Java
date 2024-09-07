a0ecec393e2154b86347c349941dafb5b4eced50
Jiatuo Zou <90865105+jiatuoz2@users.noreply.github.com>
2024-09-07 14:35:26 -0400

    fixes more bugs in depth (#3)

--- IndividualProject/src/test/java/dev/coms4156/project/individualproject/MyFileDatabaseUnitTests.java
+++ IndividualProject/src/test/java/dev/coms4156/project/individualproject/MyFileDatabaseUnitTests.java
@@ -3,6 +3,7 @@ package dev.coms4156.project.individualproject;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,6 +16,9 @@ import org.springframework.test.context.ContextConfiguration;
 @ContextConfiguration
 public class MyFileDatabaseUnitTests {
 
+  /**
+   * This method set up the testMyFileDatabase instances for testing.
+   */
   @BeforeAll
   public static void setupMyFileDatabaseForTesting() {
     testMyFileDatabase = new MyFileDatabase(0, "./data.txt");
@@ -30,16 +34,16 @@ public class MyFileDatabaseUnitTests {
     //data for coms dept
     Course coms1004 = new Course("Adam Cannon", locations[0], times[0], 400);
     coms1004.setEnrolledStudentCount(249);
-    HashMap<String, Course> courses = new HashMap<>();
+    Map<String, Course> courses = new HashMap<>();
     courses.put("1004", coms1004);
     Department compSci = new Department("COMS", courses, "Luca Carloni", 2700);
-    HashMap<String, Department> mapping = new HashMap<>();
+    Map<String, Department> mapping = new HashMap<>();
     mapping.put("COMS", compSci);
 
     testMyFileDatabase2.setMapping(mapping);
-    String expectedResult = "For the COMS department: \n" +
-        "COMS 1004: \nInstructor: Adam Cannon; Location: 417 IAB; Time: " +
-        "11:40-12:55\n";
+    String expectedResult = "For the COMS department: \n"
+        + "COMS 1004: \nInstructor: Adam Cannon; Location: 417 IAB; Time: "
+        + "11:40-12:55\n";
     assertEquals(expectedResult, testMyFileDatabase2.toString());
   }
 
@@ -48,10 +52,10 @@ public class MyFileDatabaseUnitTests {
     //data for coms dept
     Course coms1004 = new Course("Adam Cannon", locations[0], times[0], 400);
     coms1004.setEnrolledStudentCount(249);
-    HashMap<String, Course> courses = new HashMap<>();
+    Map<String, Course> courses = new HashMap<>();
     courses.put("1004", coms1004);
     Department compSci = new Department("COMS", courses, "Luca Carloni", 2700);
-    HashMap<String, Department> mapping = new HashMap<>();
+    Map<String, Department> mapping = new HashMap<>();
     mapping.put("COMS", compSci);
 
     int expectedResult = 1;
